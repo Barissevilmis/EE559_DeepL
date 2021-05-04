fd7b08bf1066674d8f688f809bcadd654e870a4c
barissevilmis <baris.sevilmis@epfl.ch>
2021-05-04 14:25:16 +0300

    Started

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-import torch as pt
+import dlc_practical_prologue as prologue
+
+train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(1000)
+
+print('train_input', train_input.size(), 'train_target', train_target.size())
+print('test_input', test_input.size(), 'test_target', test_target.size())
