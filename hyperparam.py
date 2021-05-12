a131ed27c75ad2eb7039197b868da85ac49befff
Furkan Karakas <furkan.karakas@epfl.ch>
2021-05-12 15:46:33 +0300

    Added hyperparams.

---
+++ hyperparam.py
@@ -0,0 +1,11 @@
+lr = [1e-1, 5e-2, 1e-2, 5e-3, 1e-4]
+weight_decay = [1e-3, 1e-4, 1e-5]
+batch_size = [25, 50, 100]
+aux_param = [.2, .4, .6, .8]
+
+hyperparams = {
+    "lr": lr,
+    "weight_decay": weight_decay,
+    "batch_size": batch_size,
+    "aux_param": aux_param,
+}
--- utils.py
+++ utils.py
@@ -38,7 +38,8 @@ def generate_dataset(sample_size=1000):
     Generate the dataset from prologue file
     '''
 
-    train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(sample_size)
+    train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(
+        sample_size)
     return (train_input, train_target, train_classes), (test_input, test_target, test_classes)
 
 
@@ -51,7 +52,7 @@ def preprocess_dataset(train_input, train_target, train_classes, test_input, tes
     train_input = normalize_input(train_input)
     test_input = normalize_input(test_input)
 
-    #Load into DataSet class
+    # Load into DataSet class
     train_dataset = DataSet(train_input, train_target, train_classes, True)
     test_dataset = DataSet(test_input, test_target, test_classes, False)
 
@@ -85,58 +86,61 @@ def compute_nb_errors(model, data_input, data_target, batch_size=100):
     return nb_data_errors
 
 
-def train_model(**model_hyperparams):
+def train_model(model, criterion, epochs=100, **model_hyperparams):
     '''
     Training model with provided hyperparameters
     '''
 
     # Generate dataset
-    (train_input, train_target, train_classes), (test_input, test_target, test_classes) = generate_dataset(model_hyperparams["sample_size"])
+    (train_input, train_target, train_classes), (test_input, test_target,
+                                                 test_classes) = generate_dataset(model_hyperparams["sample_size"])
 
     # Preprocess dataset
-    train_dataset, test_dataset = preprocess_dataset(train_input, train_target, train_classes, test_input, test_target, test_classes)
+    train_dataset, test_dataset = preprocess_dataset(
+        train_input, train_target, train_classes, test_input, test_target, test_classes)
 
-    #Use dataloader for shuffling and utilizing data
-    train_dataloader = utils.data.DataLoader(train_dataset, batch_size = model_hyperparams["train_batch_size"], shuffle = True)
-
-    #Send model, optimizer and criterion to corresponding device
-    model = model_hyperparams['model']
-    criterion = model_hyperparams['criterion']
+    # Use dataloader for shuffling and utilizing data
+    train_dataloader = utils.data.DataLoader(
+        train_dataset, batch_size=model_hyperparams["batch_size"], shuffle=True)
 
     device = device_choice()
     model.to(device)
     criterion.to(device)
-    
-    #Lists for train loss and accuracy values
+
+    # Lists for train loss and accuracy values
     train_losses = list()
     train_acc = list()
     test_acc = list()
 
-    #Scheduler used for learning rate decay: Every 25 epochs lr = lr * gamma
-    optimizer = optim.Adam(model.parameters(), lr = model_hyperparams["lr"], weight_decay=1e-5)
+    # Scheduler used for learning rate decay: Every 25 epochs lr = lr * gamma
+    optimizer = optim.Adam(
+        model.parameters(), lr=model_hyperparams["lr"], weight_decay=model_hyperparams["weight_decay"])
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.1)
+    # * Playable hyperparameters: weight decay, learning rate
 
     model.train(True)
-    for epoch in range(model_hyperparams["epochs"]):
+    for epoch in range(epochs):
         epoch_loss = 0
         for batch_id, train_batch in enumerate(train_dataloader):
-            #Auxiliary loss network
+            # Auxiliary loss network
             if type(model).__name__ == "AuxiliaryNet":
                 optimizer.zero_grad()
                 pred, d1, d2 = model(train_batch['input'])
 
-                #Actual objective loss
+                # Actual objective loss
                 loss_main = criterion(pred, train_batch['target'])
-                #Auxiliary losses
+                # Auxiliary losses
                 loss_aux_1 = criterion(d1, train_batch['class1'])
                 loss_aux_2 = criterion(d1, train_batch['class2'])
-                #Final loss is a weighted sum of all losses
-                loss = model_hyperparams['aux_param1'] * loss_main + model_hyperparams['aux_param2'] * loss_aux_1 + model_hyperparams['aux_param2'] * loss_aux_2 
+                # Final loss is a weighted sum of all losses
+                loss = model_hyperparams['aux_param'] * loss_main + (1-model_hyperparams['aux_param'])/2 * \
+                    loss_aux_1 + \
+                    (1-model_hyperparams['aux_param'])/2 * loss_aux_2
 
                 loss.backward()
                 optimizer.step()
 
-            #Convolutional or Dense network
+            # Convolutional or Dense network
             else:
                 optimizer.zero_grad()
                 pred = model(train_batch['input'])
@@ -145,45 +149,31 @@ def train_model(**model_hyperparams):
                 optimizer.step()
 
             epoch_loss += loss.item()
-            print("Epoch", str(epoch), ", Batch", str(batch_id)," train loss:", str(loss.item()), "Epoch Loss Summation:",str(epoch_loss))
+            print("Epoch", str(epoch), ", Batch", str(batch_id), " train loss:", str(
+                loss.item()), "Epoch Loss Summation:", str(epoch_loss))
 
-        #Decay learning rate with scheduler
+        # Decay learning rate with scheduler
         scheduler.step()
         print("Epoch", str(epoch), ", Current learning rate:", scheduler.get_lr())
         train_losses.append(epoch_loss)
 
-        train_error = compute_nb_errors(model, train_dataset.get_data(), train_dataset.get_target(), batch_size=100)
+        train_error = compute_nb_errors(
+            model, train_dataset.get_data(), train_dataset.get_target(), batch_size=100)
         train_acc.append(1-(train_error/train_target.shape[0]))
 
-        test_error = compute_nb_errors(model, test_dataset.get_data(), test_dataset.get_target(), batch_size=100)
+        test_error = compute_nb_errors(
+            model, test_dataset.get_data(), test_dataset.get_target(), batch_size=100)
         test_acc.append(1-(test_error/test_target.shape[0]))
 
     model.train(False)
 
     return train_losses, train_acc, test_acc
 
-    
 
-def exec_model(**model_hyperparams):
+def tune_model(**model_hyperparams):
     '''
-    Execute the model by calling train function and compute accuracy values
+    This function executes a given model for various hyperparameters
     '''
-    # Train the model and record the train errors at each epoch and accuracy
-    train_loss, train_acc = train_model(**model_hyperparams)
-    # Calculate errors from training and return pure accuracy values
-    train_error = compute_nb_errors(model_hyperparams["model"], model_hyperparams["train_input"],
-                                    model_hyperparams["train_target"], model_hyperparams["batch_size"])
-    train_acc = 1-(train_error/model_hyperparams["sample_size"])
-    test_error = compute_nb_errors(
-        model_hyperparams["model"], model_hyperparams["test_input"],
-        model_hyperparams["test_target"], model_hyperparams["batch_size"])
-    test_acc = 1-(test_error/model_hyperparams["sample_size"])
-
-    return train_acc, test_acc
-
-
-def tune_model():
-    pass
 
 
 def stats_model():
@@ -191,20 +181,22 @@ def stats_model():
 
 
 def plot_train_test(loss, accuracy):
-    fig, ax1 = plt.subplots(figsize=(8,8))
+    fig, ax1 = plt.subplots(figsize=(8, 8))
 
     # Plot train loss
     ax1.set_title('Training Loss and Test Accuracy', fontsize=16)
     ax1.set_xlabel('Epochs', fontsize=16)
     ax1.set_ylabel('Train loss (log)', fontsize=16)
     ax1.set_yscale("log")
-    ax1 = sns.lineplot(x=range(len(loss)), y=loss, color='tab:orange', label='Train loss', legend=False)
+    ax1 = sns.lineplot(x=range(len(loss)), y=loss,
+                       color='tab:orange', label='Train loss', legend=False)
     ax1.tick_params(axis='y')
     ax2 = ax1.twinx()
 
     # Plot test accuracy
     ax2.set_ylabel('Test accuracy (%)', fontsize=16)
-    ax2 = sns.lineplot(x=range(len(accuracy)), y=accuracy, color = 'tab:green', label='Test accuracy', legend=False)
+    ax2 = sns.lineplot(x=range(len(accuracy)), y=accuracy,
+                       color='tab:green', label='Test accuracy', legend=False)
     ax2.tick_params(axis='y')
 
     # Set legend
@@ -213,6 +205,6 @@ def plot_train_test(loss, accuracy):
     lines = line1 + line2
     labels = label1 + label2
     ax1.legend(lines, labels, loc='center right')
-    
+
     plt.savefig('loss_acc.png', dpi=800, transparent=True)
     plt.show()
