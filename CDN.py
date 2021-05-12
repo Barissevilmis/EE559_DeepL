f2b595942e96ae089a6e7df1e140afff48a41b8c
Furkan Karakas <furkan.karakas@epfl.ch>
2021-05-12 11:51:00 +0300

    Work.

--- CDN.py
+++ CDN.py
@@ -9,24 +9,23 @@ from FC import DenseNet
 
 class ConvDenseNet(nn.Module):
 
-    '''   
+    '''
     Merging Convolutional and Dense Networks
     (Operations)                                    (Input Size)
     -------------------------------------------------------------
     Initial input :                                  2 x 14 x 14
-    
+
     '''
 
     def __init__(self):
         super(ConvDenseNet, self).__init__()
         self.convnet = ConvNet()
         self.fcnet = DenseNet()
-       
-   
+
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2, stride=2)
         x = self.dropout1(self.bn1(x))
         x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2, stride=2)
         x = self.dropout2(self.bn2(x))
         x = F.relu(self.fc1(x.view(-1, 256)))))
-        return F.softmax(self.fc2(x), dim = 1)
+        return F.softmax(self.fc2(x), dim=1)
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import torch
-import numpy as np
-from torch.utils import data
+import torch.nn as nn
 import dlc_practical_prologue as prologue
 
 
@@ -22,12 +21,12 @@ def device_choice():
         return torch.device("cpu")
 
 
-def generate_dataset(sample=1000):
+def generate_dataset(sample_size=1000):
     """Generate the dataset from prologue file
     """
 
     train_input, train_target, train_classes, test_input, test_target, test_classes = prologue.generate_pair_sets(
-        sample)
+        sample_size)
     return (train_input, train_target, train_classes), (test_input, test_target, test_classes)
 
 
@@ -69,10 +68,67 @@ def preprocess_dataset(train_input, train_target, train_classes, test_input, tes
     return (train_input, train_target, train_classes), (test_input, test_target, test_classes)
 
 
+def compute_nb_errors(model, data_input, data_target, batch_size=128):
+    """Calculate the number of errors given a model, data input, data target and mini batch size.
+    Taken from practical exercises
+    """
+
+    nb_data_errors = 0
+
+    for b in range(0, data_input.size(0), batch_size):
+        output = model(data_input.narrow(0, b, batch_size))
+
+        if type(model).__name__ ==
+
+        _, predicted_classes = torch.max(output, 1)
+        for k in range(batch_size):
+            if data_target[b + k] != predicted_classes[k]:
+                nb_data_errors += 1
+
+    return nb_data_errors
+
+
+def train_model():
+    pass
+
+
+def exec_model(**model_hyperparams):
+    '''Execute the model given model and hyperparameters'''
+
+    # Generate
+    (train_input, train_target, train_classes), (test_input, test_target,
+                                                 test_classes) = generate_dataset(model_hyperparams["sample_size"])
+    # Preprocess
+    (train_input, train_target, train_classes), (test_input, test_target, test_classes) = preprocess_dataset(
+        (train_input, train_target, train_classes), (test_input, test_target, test_classes))
+    # Train the model and record the train errors at each epoch and accuracy
+    train_loss, train_acc = train_model(**model_hyperparams)
+    # Calculate errors from training and return pure accuracy values
+    train_error = compute_nb_errors(model_hyperparams["model"], model_hyperparams["train_input"],
+                                    model_hyperparams["train_target"], model_hyperparams["batch_size"])
+    train_acc = 1-(train_error/model_hyperparams["sample_size"])
+    test_error = compute_nb_errors(
+        model_hyperparams["model"], model_hyperparams["test_input"],
+        model_hyperparams["test_target"], model_hyperparams["batch_size"])
+    test_acc = 1-(test_error/model_hyperparams["sample_size"])
+
+    return train_acc, test_acc
+
+
+def tune_model():
+    pass
+
+
+def stats_model():
+    pass
+
+
 if __name__ == "__main__":
 
     (train_input, train_target, train_classes), (test_input,
                                                  test_target, test_classes) = generate_dataset()
-    (train_input, train_target, train_classes), (test_input,
-                                                 test_target, test_classes) = preprocess_dataset(train_input, train_target, train_classes, test_input, test_target, test_classes)
-    print(train_input)
+    print(train_input.size())
+    print(train_input[0].shape)
+    # (train_input, train_target, train_classes), (test_input,
+    #                                            test_target, test_classes)=preprocess_dataset(train_input, train_target, train_classes, test_input, test_target, test_classes)
+    # print(train_input)
