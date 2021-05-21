b3614bdddbd7fab73acf556d78f9954a8a2afc43
barissevilmis <baris.sevilmis@epfl.ch>
2021-05-21 12:56:35 +0200

    Optimizers done

--- Proj2/test.py
+++ Proj2/test.py
@@ -24,3 +24,15 @@ sigmoid_model = Sequential(Linear(2, 25), Sigmoid(),
                            Linear(25, 25), Sigmoid(),
                            Linear(25, 25), Sigmoid(),
                            Linear(25, 1), Sigmoid())
+
+model_params_adam = {'lr':[1e-2,1e-3,1e-4], 'beta1': [0.2, 0.6, 0.9], 'beta2': [0.8, 0.9, 0.999], 'weight_decay': [0, 5e-9, 1e-8]}
+model_params_sgd = {'lr':[1e-2,1e-3,1e-4,1e-5]}
+
+best_param_adam_relu = hyperparameter_tuning(relu_model, optimizer="adam", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_adam)
+best_param_adam_tanh = hyperparameter_tuning(tanh_model, optimizer="adam", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_adam)
+best_param_adam_sigmoid = hyperparameter_tuning(sigmoid_model,  optimizer="adam", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_adam)
+
+
+best_param_sgd_relu = hyperparameter_tuning(relu_model, optimizer="sgd", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_sgd)
+best_param_sgd_tanh= hyperparameter_tuning(tanh_model, optimizer="sgd", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_sgd)
+best_param_sgd_sigmoid = hyperparameter_tuning(sigmoid_model,  optimizer="sgd", criterion=MSE(), epochs=50, batch_size=100, sample_size=1000, rounds=10, **model_params_sgd)
