f66dc6209f141a6db4b3d0b46396ac0df8c71d4f
barissevilmis <baris.sevilmis@epfl.ch>
2021-05-22 13:35:40 +0200

    Optimizers done

--- Proj2/optimizers.py
+++ Proj2/optimizers.py
@@ -61,17 +61,17 @@ class _Optimizer_:
         return self.model, epoch_losses
 
     def step(self):
-        self.model.step(self.lr)
+        pass
 
 
 class SGDOptimizer(_Optimizer_):
     '''
     Mini batch SGD optimizer: Only  parameters learning rate
-    Learning rate: 1e-2 by default
+    Learning rate: 5e-1 by default
     Optimize by step(): decrease by learning rate * gradient
     '''
 
-    def __init__(self, model, epochs=100, criterion=MSE(), batch_size=1, lr=1e-2):
+    def __init__(self, model, epochs=100, criterion=MSE(), batch_size=1, lr=5e-1):
 
         if(lr < 0.0):
             self.lr = 1e-2
@@ -87,3 +87,60 @@ class SGDOptimizer(_Optimizer_):
             self.batch_size = batch_size
 
         super().__init__(model, epochs, criterion, batch_size, lr)
+
+    def step(self):
+        for (w, b, gw, gb) in self.model.param():
+            w -= self.lr * gw
+            b -= self.lr * gb
+
+
+class AdamOptimizer(_Optimizer_):
+    '''
+    Adam optimizer: Parameters learning rate, beta1, beta2, weight_decay, epsilon
+    Learning rate: 1e-1 by default
+    Optimize by step(): decrease by learning rate * gradient
+    '''
+
+    def __init__(self, model, epochs=100, criterion=MSE(), batch_size=1, lr=1e-1, beta1 = 0.9, beta2 = 0.999, weight_decay = 0.0, epsilon = 1e-8):
+
+        if(lr < 0.0):
+            self.lr = 5e-1
+            print(
+                "Learning rate set to default (5e-1) due to negative learning rate input!")
+        else:
+            self.lr = lr
+
+        if(beta1 < 0.0):
+            self.beta1 = 0.9
+            print("Beta1 set to default (0.9) due to negative beta1 input!")
+        else:
+            self.beta1= beta1
+
+        if(beta2 < 0.0):
+            self.beta2 = 0.999
+            print("Beta2 set to default (0.999) due to negative beta2 input!")
+        else:
+            self.beta2 = beta2
+
+        if(weight_decay < 0.0):
+            self.weight_decay = 0.0
+            print("Weight decay set to default (0.0) due to negative weight decay input!")
+        else:
+            self.weight_decay = weight_decay
+
+        if(epsilon < 0.0):
+            self.epsilon = 1e-8
+            print("Epsilon set to default (1e-8) due to negative epsilon input!")
+        else:
+            self.epsilon = epsilon
+
+        if(batch_size < 0):
+            self.batch_size = 1
+            print("Mini batch size set to default (1) due to negative batch size input!")
+        else:
+            self.batch_size = batch_size
+
+        super().__init__(model, epochs, criterion, batch_size, lr)
+
+    def step(self):
+        self.model.step(self.lr)
